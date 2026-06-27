ff189d2171304d334f37ce6e64cf1bffee023fc0
Medun <donhuanmatus62@gmail.com>
2026-06-27 23:16:11 +0300

    Add terraform.tfvars and variables.tf

--- resortsweatherapi.tf
+++ resortsweatherapi.tf
@@ -3,12 +3,12 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "fastapi-rg"
-  location = "West Europe"
+  name     = var.resource_group_name
+  location = var.location
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "fastapireg12345"
+  name                = var.acr_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
@@ -16,15 +16,15 @@ resource "azurerm_container_registry" "acr" {
 }
 
 resource "azurerm_service_plan" "plan" {
-  name                = "fastapi-plan"
+  name                = var.service_plan_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   os_type             = "Linux"
-  sku_name            = "B1"
+  sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "app" {
-  name                = "fastapi-app-12345"
+  name                = var.app_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   service_plan_id     = azurerm_service_plan.plan.id
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+acr_name   = "fastapireg12345"
+app_name   = "fastapi-app-12345"
